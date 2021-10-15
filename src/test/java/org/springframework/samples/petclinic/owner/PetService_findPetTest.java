7ae2dacbb9f744ca3f234ea5399e0f95c7e2475b
Mehrnaz Shams <mshamsabadi78@gmail.com>
2021-10-15 17:43:37 +0330

    Correct PetService_findPetTest

--- src/test/java/org/springframework/samples/petclinic/owner/PetService_findPetTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetService_findPetTest.java
@@ -1,31 +1,55 @@
 package org.springframework.samples.petclinic.owner;
 
-import org.junit.runner.RunWith;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.Before;
+
 import org.junit.runners.Parameterized;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.springframework.samples.petclinic.utility.PetTimedCache;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(Parameterized.class)
 public class PetService_findPetTest {
-	//	public int petId;
-//	public Pet pet = new Pet();
-//	Pet cat = new Pet();
-//
-//	public PetServiceTest (int id, Pet expected) {
-//		this.petId = id;
-//		this.pet = expected;
-//		cat.setId(12);
-//	}
-//
-//	@Parameterized.Parameters
-//	public static Collection<Object[]> parameters() {
-//		return Arrays.asList (new Object [][] {{12, cat}, {2, null}});
-//	}
-//
-//	@Test public void Pet_found_correctly() {
-//		PetTimedCache pets = mock(PetTimedCache.class);
-//		OwnerRepository owners = mock(OwnerRepository.class);
-//		Logger log = mock(Logger.class);
-//		PetService petService = new PetService(pets, owners, log);
-//		when(pets.get(12)).thenReturn(cat);
-//		assertTrue (pet == cat);
-//	}
+
+	private int petId;
+	private Pet pet;
+
+	public static Pet foundPet1 = new Pet();
+	public static Pet foundPet2 = new Pet();
+
+	public PetService_findPetTest(int petId, Pet pet) {
+		this.petId = petId;
+		this.pet = pet;
+	}
+
+	@Before
+	public void setUp() {
+		foundPet1.setId(12);
+		foundPet2.setId(34);
+	}
+
+	@Parameterized.Parameters
+	public static Collection parameters() {
+		return Arrays.asList(new Object[][] {
+			{ 12, foundPet1},
+			{ 34, foundPet2}
+		});
+	}
+
+	@Test
+	public void Pet_found_correctly() {
+		OwnerRepository owners = mock(OwnerRepository.class);
+		Logger logger = mock(Logger.class);
+		PetTimedCache pets = mock(PetTimedCache.class);
+		PetService petService = new PetService(pets, owners, logger);
+		when(pets.get(petId)).thenReturn(pet);
+		assertEquals(petService.findPet(petId).getId(), pet.getId());
+	}
 }
